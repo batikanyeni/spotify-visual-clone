11d391333a421c5bf61e30089e1aa861ae624cb5
batikanyeni <batikanyeni@outlook.com>
2023-08-27 19:58:30 +0300

    premium-page update

--- src/components/pages/PremiumPage.js
+++ src/components/pages/PremiumPage.js
@@ -19,5 +19,38 @@ export const PremiumPage = () => {
             </div>
             <img alt='premium' src='./playlist_images/premium.png'/>
         </div>
+        <div className={classes['middle-info']}>
+            <h1>Why go Premium?</h1>
+            <div className={classes['advantages-container']}>
+                <div className={classes['advantage']}>
+                    <img alt='download-music' src='./icons/downloadMusic.png'/>
+                    <div>
+                        <p className={classes['p1']}>Download Music.</p>
+                        <p className={classes['p2']}>Listen anywhere.</p>
+                    </div>
+                </div>
+                <div className={classes['advantage']}>
+                    <img alt='download-music' src='./icons/ad-free.png'/>
+                    <div>
+                        <p className={classes['p1']}>Ad-free music listening.</p>
+                        <p className={classes['p2']}>Enjoy nonstop music..</p>
+                    </div>
+                </div>
+                <div className={classes['advantage']}>
+                    <img alt='download-music' src='./icons/any-song.png'/>
+                    <div>
+                        <p className={classes['p1']}>Play any song.</p>
+                        <p className={classes['p2']}>Even on mobile.</p>
+                    </div>
+                </div>
+                <div className={classes['advantage']}>
+                    <img alt='download-music' src='./icons/unlimited-skip.png'/>
+                    <div>
+                        <p className={classes['p1']}>Unlimited skips.</p>
+                        <p className={classes['p2']}>Just hit next.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 }
